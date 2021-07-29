615e315c4fa140ed9de3c9b984b98be40309e737
ERACKNET-suz <80479885+ERACKNET-suz@users.noreply.github.com>
2021-07-29 13:53:43 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
-exports.Prefix = `!`;
-exports.Token = `your bot token`;
+exports.Prefix = `?`;
+exports.Token = `ODUxNzYyMjgwODExMDY5NDQw.YL8_cA.mV-lB5eK0a3G2qRxm6JNOjSYSvM`;
 exports.Color = `RANDOM`;
+
